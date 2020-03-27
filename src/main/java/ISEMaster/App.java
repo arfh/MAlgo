e7e6638590e8f8d714bc881dcb08cffda4e5a022
Alex <alexander.reuter@alumni.fh-aachen.de>
2020-03-27 14:42:10 +0100

    initial Commit

---
+++ src/main/java/ISEMaster/App.java
@@ -0,0 +1,13 @@
+package ISEMaster;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
