300dbea4c6f766b5429672c6cdf65feb3c065257
alex <Alexander.Reuter@alumni.fh-aachen.de>
2020-04-10 15:39:38 +0200

    Cleanup

--- src/main/java/ISEMaster/Algorithm.java
+++ src/main/java/ISEMaster/Algorithm.java
@@ -95,7 +95,7 @@ public class Algorithm {
     private static void addNewNodesToTree(Edge e, Node[] newNodes, ArrayList<Edge> edges) {
         Node a1 = createNewNodeIfNotExists(e.getA(), newNodes);
         Node b1 = createNewNodeIfNotExists(e.getB(), newNodes);
-        addEdgeToList(edges, new Edge(a1, b1, e.getCosts()));
+        edges.add(new Edge(a1, b1, e.getCosts()));
     }
 
     private static Node createNewNodeIfNotExists(Node a, Node[] newNodes) {
@@ -103,10 +103,6 @@ public class Algorithm {
             newNodes[a.getLabel()] = new Node(a.getLabel());
         }
         return newNodes[a.getLabel()];
-    }
-
-    private static void addEdgeToList(ArrayList<Edge> edges, Edge e) {
-        edges.add(e);
     }
 
 }
